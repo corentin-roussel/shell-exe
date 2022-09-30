2d3a61152c9c4a0fe763281db4a1588daba59685
corentin-roussel <corentin.roussel@laplateforme.io>
2022-09-30 16:42:00 +0200

    rendu myloop boucle

---
+++ Job7/myloop.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+#boucle for i=0 i<10 plus petit que 10 i++ pour l'incrementation et echo la variable et done pour fermer la boucle
+for (( i=0; i<10; i++ ));do
+	echo "Je suis un script qui arrive a faire une boucle $i"
+done
