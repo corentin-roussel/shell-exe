350e5371a764849fe7f32b15d16e267526d132ed
corentin-roussel <corentin.roussel@laplateforme.io>
2022-10-06 11:56:16 +0200

    passage en chpasswd

--- Job9/accessrights.sh
+++ Job9/accessrights.sh
@@ -3,14 +3,15 @@
 file="/home/corentin/shell-exe/Job9/Shell_Userlist.csv"
 
 #utilisation de ifs pour lire les lignes du fichier csv
-while IFS=, read -r Id Prenom Nom Mdp Role
+while IFS="," read -r Id Prenom Nom Mdp Role
 do
 #if vérifie id n'est pas un nombre pour skip la ligne
-    if [ "$Id" = "$Id" ] 2>/dev/null;
+    if [ "$Id" -eq "$Id" ] 2>/dev/null;
 #then useradd pour ajouter les utilisateur probléme d'Id
         then 
-            useradd -u $Id -p $Mdp ${Prenom,,} 
-#pas essayer vu que probléme a la ligne d'avant
+            useradd -u $Id -m ${Prenom,,} 
+            echo $Prenom:$Mdp | chpasswd
+
         if [ ${Role:0:5}  = "Admin" ];
             then 
                 usermod -aG sudo ${Prenom,,}
